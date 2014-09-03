e9def677032f5a6d9a7ac1a9bd8310bced7ccc56
lnmx <len@lnmx.org>
2014-09-03 16:02:05 -0400

    log http requests

--- http-hello/unikernel.ml
+++ http-hello/unikernel.ml
@@ -10,8 +10,19 @@ module Main (C:CONSOLE) (S:Cohttp_lwt.Server) = struct
       S.respond_string ~status:`OK ~body:"hello world!\n" ()
     in
 
+    let log request =
+        let meth = Cohttp.Code.string_of_method (S.Request.meth request) in
+        let path = Uri.path (S.Request.uri request) in
+        let vers = Cohttp.Code.string_of_version (S.Request.version request) in
+        C.log c (Printf.sprintf "http: %s %s %s" meth path vers);
+
+        let headers = Cohttp.Header.to_list (S.Request.headers request) in
+        List.iter (fun (k,v) -> C.log c (Printf.sprintf "    %s: %s" k v) ) headers
+    in
+
     (* HTTP callback *)
     let callback conn_id request body =
+      log request;
       handler request body
     in
 
